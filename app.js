f47126671a13e191c32a8dbba52d0d92aaba9332
Soumya Gupta <71893528+SoumyaGupta025@users.noreply.github.com>
2021-09-04 17:31:16 +0530

    update app.js

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ app.post("/", function (req, res){
   });
 })
 
-app.listen(3000, function () {
+app.listenprocess.env.PORT || 3000,function () {
   console.log("Server is running at port 3000");
 });
